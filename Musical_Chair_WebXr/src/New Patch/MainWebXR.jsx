e072fdf388d8b2b91aef297deb947834886eb68a
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-03-01 17:21:36 +0530

    Add files via upload

--- Musical_Chair_WebXr/src/New Patch/MainWebXR.jsx
+++ Musical_Chair_WebXr/src/New Patch/MainWebXR.jsx
@@ -1,6 +1,10 @@
-//WID;//Impl(28/2/2026)(By Sarthak Mittal(DegamieSign))#1.1.1.1.1.1.1.1.1.1.c1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1
+//WID;//Impl(1/03/2026)(By Sarthak Mittal(DegamieSign))#1.1.1.1.1.1.1.1.1.1.c1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1.1
 
         Texture mdlText=null;mdlrot=0;
+        existsByMdlText(mdlText){
+            if(mdlText!=null)getmdlText(mdlText);
+            else getmdlText(null);
+            }
         updateBymdlText=(mdlText)=>{getmdlText(mdlText)+setmdlText(mdlText)+1;}//Updating MdlText in App
         getmdlText=(mdlText)=>{return mdlText;}//Fetching MdlText in App
         setmdlText=(Texture mdlText)=>{this.mdlText=mdlText;}//Binding mdlText in App
