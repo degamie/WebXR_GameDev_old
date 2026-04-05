a76e8619a4ddb24f06ec6d38f0a7e05f7b44b176
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-05 21:53:24 +0530

    Add files via upload

--- Musical_Chair_WebXr/src/New Patch/Auth.jsx
+++ Musical_Chair_WebXr/src/New Patch/Auth.jsx
@@ -1,1 +1,5 @@
+//WID(5/4/2026)(Sarthak Mittal)(DegamieSign)#1.1
+const setUserName=(Username)=>{this.Username=Username;}
+cosnt getPassword=(password)=>{return password;}//Fethching password in App
 const Username=new Username();
+const getUserName=(Username)=>{return Username;}//Fetching Username in App
