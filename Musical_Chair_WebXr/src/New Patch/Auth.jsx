47753584413f73ed9dce9f34e9c8b14436e255ac
Degamie_sign <106448560+degamie@users.noreply.github.com>
2026-04-26 22:40:32 +0530

    Add files via upload

--- Musical_Chair_WebXr/src/New Patch/Auth.jsx
+++ Musical_Chair_WebXr/src/New Patch/Auth.jsx
@@ -1,4 +1,27 @@
-//WID(17/4/2026)(Sarthak Mittal)(DegamieSign)#1.1.1,1c.1.1.1.1cc.1.1.1.1.1.1.1,1.1  .1.1/1
+//WID(26/4/2026)(Sarthak Mittal)(DegamieSign)#1.1.1,1c.1.1.1.1cc.1.1.1.1.1.1.1,1.1  .1.1/1.1.1.1.1.1/.1.1.1/1/1userVal.1.1s/1.1/1.1/1/1/1.1
+const setUserlogoutTime=(userlogoutTime)=>{this.userlogoutTime=userlogoutTime;}//Bidning UserLogoutTime in App
+const getuserlogoutTime=(userlogoutTime)=>{return userlogoutTime;}
+const userlogoutTime=0;
+const existsByUserInTime=(userInTime)=>{if(userInTime!==0)getUserInTime(userInTime);else getUserInTime(null);}//Checking UserIntImt in App
+const updateByUserInTime=(userInTime)=>{getUserInTime(userInTime)+setUserInTime(userInTime)+1;}//Updating UserInTIme iun app
+const setUserInTime=(userInTime)=>{this.userInTime=userInTime;}//binding in UserTime in App
+const getUserInTime=(userInTime)=>{return userInTime;}
+const userinTime=(userInTime)=>{if(userInTime!==null)getUserInTime(userInTime);else getUserInTime(null);}//Checking UserInTime in App
+const existsByUserVal=(userVal)=>{if(userVal!==0)getUserval(userVal);else getUserval(0);}
+const updateByUserVal=(userVal)=>{getUserval(userVal)+setUserVal(userVal)+1;}//binding UserVal in App
+const getUserval=(userVal)=>{return userVal;}//Fetching UserVal in App
+const setUserVal=(userVal)=>{this.userVal=userVal;}//binding USerVal in  App
+const userval;
+const existsByuserCnt=(userCount)=>{if(userCount!==0)getuserCount(userCount);else getuserCount(0);}//checking userCount in App
+const updateByuserCount=(userCount)=>{getuserCount(userCount)+setUserCount(userCount)+1;}//updating USerCnt in App
+ const userCount=0;
+ const existsByUserNum=(userNum)=>{if(userNum!==0)getUserNum(userNum);else getUserNum(0);}//binding UserNum in App
+const updateByuserCount=(userCount)=>{getuserCount(userCount)+setUserCount(userCount)+1;}//Up;dating Usercount in App
+const getuserCount=(usercnt)=>{return usercnt;}//Fethcing UserCnt in App
+const setUserCount=(userCount)=>{this.userCount=userCount;}//binding userCount in app
+const existsByUSerId=(userId)=>{
+    if(userId!==null)getUserId(userId);
+    else getUserId(null);}
 const updateByUserId=(userId)=>{
     if(UserId!==null)getUserId(userId);
     else getUserId(null);
@@ -10,6 +33,7 @@ const existsByUserName=(userName)=>{
 const updateByUserName=(userName)=>{getUserName(userName)+setUserName(userName)+1;}//updating UserName inapp
  const setUserName=(userName)=>{this.userName=userName;}//binding UserName in App
 const existsByUserNum=(userNum)=>{if(userNum!==0)getUserNum(userNum);else getUserNum(0);}//Checking userNum in App
+
 const updateByUserNum=(userNum)=>{getUserNum(userNum)+setUserNum(userNum)+1;}//Updating UserNum in App
 const setUserNum=(userNum)=>{this.userNum=userNum;}//Binding UserNum in App
 const getUserNum=(userNum)=>{return userNum;}//Fethcing UserNum in App
