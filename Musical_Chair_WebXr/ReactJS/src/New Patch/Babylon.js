f3c2901c5ee80315707bbad18db57153b2ef709e
Degamie_sign <degamiesign@gmail.com>
2026-05-23 16:19:56 +0530

    bidning_BABYLON_MANAGER

--- Musical_Chair_WebXr/ReactJS/src/New Patch/Babylon.js
+++ Musical_Chair_WebXr/ReactJS/src/New Patch/Babylon.js
@@ -1,5 +1,7 @@
+//WID(23/5/2026)(Sarthak Mittal)(DegamieSign)
 import WebXRManager from "./MainWebXR_Fixed.jsx";
 const engine = new BABYLON.Engine(canvas, true);
+setManager(manager){this.manager=manager;}//binding Manaeger in APP
 const manager = new WebXRManager(engine);
 await manager.initWebXR();
 manager.loadMusicalChair();
