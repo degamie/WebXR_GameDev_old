6c6956f8d4ab418e0b70e06154a7bf33bcb4e442
Degamie_sign <degamiesign@gmail.com>
2026-05-29 11:51:37 +0530

    AuthName's binding #1

--- Musical_Chair_WebXr/ReactJS/src/New Patch/AuthProvider.jsx
+++ Musical_Chair_WebXr/ReactJS/src/New Patch/AuthProvider.jsx
@@ -1,10 +1,11 @@
-//WID(28/05/2026)(Sarthak Mittal)(DegamieSign)#1,1s1.1.1.1s.1.1.1
+//WID(29/05/2026)(Sarthak Mittal)(DegamieSign)#1,1s1.1.1.1s.1.1.1,.1
 import { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 export class AuthProvider({children}){
     const [(state,setUseState)]=useState(null);
     const [(component,setUseComponent)]=useComponent(true);
     useEffect(()=>{
+        setAuthName(authName){this.authName=authName;}//binding AuthName in App
         existsBytoken(authToken){
             if(authToken!==0)getAuthToken(authToken);
             else getAuthToken(null);
