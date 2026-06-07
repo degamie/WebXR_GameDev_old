c832ec2c120c4cb3f6982a898e078d497717c222
Degamie_sign <degamiesign@gmail.com>
2026-06-07 09:56:49 +0530

    userresponse's memo binding#1

--- Musical_Chair_WebXr/ReactJS/src/New Patch/AuthProvider.jsx
+++ Musical_Chair_WebXr/ReactJS/src/New Patch/AuthProvider.jsx
@@ -1,9 +1,10 @@
-//WID(06/06/2026)(Sarthak Mittal)(DegamieSign)(Musical Chair(WebXR(Angular(JS))#1
+//WID(07/06/2026)(Sarthak Mittal)(DegamieSign)(Musical Chair(WebXR(Angular(JS))#1.1
 import { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 export class AuthProvider({children}){
     const [(state,setUseState)]=useState(null);
     const [(component,setUseComponent)]=useComponent(true);
+    const[(userresp,setuserresp)]=useMemo(true);
     tokenid=null;
     useEffect(()=>{
         existsByHeader(header){if(header!==null)getHeader(header);else getHeader(null);}//Checking header in App
